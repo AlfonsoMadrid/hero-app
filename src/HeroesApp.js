f80332e7723f8cc403ae51a19054c40e96fa2fd2
AlfonsoMadrid <alfonso.madrid@meir.cl>
2021-08-09 08:54:37 -0400

    latest version updated

--- src/HeroesApp.js
+++ src/HeroesApp.js
@@ -3,6 +3,8 @@ import { AuthContext } from './auth/AuthContext';
 import { authReducer } from './auth/authReducer';
 import { AppRouter } from './routers/AppRouter';
 
+import './css/heroesApp.css'
+
 const init = () => {
     return JSON.parse(localStorage.getItem('user')) || { logged: false };
 }
@@ -17,9 +19,11 @@ export const HeroesApp = () => {
 
     return (
 
-        <AuthContext.Provider value={{ user, dispatch }}>
-            <AppRouter />
-        </AuthContext.Provider>
+        
+            <AuthContext.Provider value={{ user, dispatch }}>
+                <AppRouter />
+            </AuthContext.Provider>
+    
 
 
     )
